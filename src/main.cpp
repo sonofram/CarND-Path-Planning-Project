2f5a74db50fa8b51096c6fd9348015f11e001de3
sonofram <sonofram@gmail.com>
2017-10-31 20:47:03 -0700

    submitting project

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <fstream>
-#include <math.h>
 #include <uWS/uWS.h>
 #include <chrono>
 #include <iostream>
@@ -8,8 +7,22 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
+//#include <algorithm>
+//#include <math.h>
+//#include "CostFunction.cpp"
 
 using namespace std;
+
+//CostFunction cf;
+int curr_lane;
+int PREFERRED_BUFFER_DISTANCE = 20.00;
+double PREFERRED_VELOCITY = 22;
+double curr_lead_vehicle_speed = 22;
+double target_vehicle_speed;
+vector<double> avg_scores = {0,0,0};
+int AVERAGE_MAX_NUM = 5;
+map<int,vector<double>> lane_wise_score;
 
 // for convenience
 using json = nlohmann::json;
@@ -159,6 +172,315 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
+//#######################################################################################################
+
+int getD(int lane){
+	if(lane == 0){
+		return 2;
+	}else if(lane == 1) {
+		return 6;
+	}else{
+		return 10;
+	}
+}
+
+int getLaneNumber(double d) {
+
+  if (d < 4) {
+    return 0;
+  } else if (d < 8) {
+    return 1;
+  } else {
+    return 2;
+  }
+}
+
+vector<double> closestVehicle(double s, int lane, vector<vector<double>> sensor_fusion, bool front_back) {
+	  double distance = 10000;
+	  double velocity = PREFERRED_VELOCITY;
+	  double vehicle_s;
+	  double vehicle_d;
+	  double vehicle_v;
+	  int vehicle_lane;
+	  double vehicle_id;
+
+	  // Check each vehicle in sensor range
+	  // Verify the closes car based on the s value. Pick up one with
+	  // closest s value.
+
+	  for(int vehicle = 0; vehicle < sensor_fusion.size(); vehicle++) {
+	    vehicle_s = sensor_fusion[vehicle][5];
+	    vehicle_d = sensor_fusion[vehicle][6];
+	    vehicle_v = sqrt(pow(sensor_fusion[vehicle][3], 2)+pow(sensor_fusion[vehicle][4], 2));
+	    vehicle_lane = getLaneNumber(vehicle_d);
+	    vehicle_id = sensor_fusion[vehicle][0];
+
+	    if (vehicle_lane == lane) { // if same lane
+	      if (front_back == true) {
+	        if (vehicle_s > s and (vehicle_s - s) < distance) { // check for front closest vehicle
+	          distance = vehicle_s - s;
+	          velocity = vehicle_v;
+	        }
+	      } else {
+	        if (s >= vehicle_s and (s - vehicle_s) <= distance) { // check for rare closest vehicle
+	          distance = s - vehicle_s;
+	          velocity = vehicle_v;
+	        }
+	      }
+	    }
+	  }
+
+	  if (distance <= 0) { // Avoid dividing by zero in laneScore()
+	    distance = 1.0;
+	  }
+
+	  if (lane == curr_lane and front_back == true) {
+	    curr_lead_vehicle_speed = velocity;
+	  }
+
+	  return {distance, velocity, vehicle_id};
+
+}
+
+double logistic(double x){
+  // A function that returns a value between 0 and 1 for x in the range[0, infinity] and - 1 to 1 for x in
+  // the range[-infinity, infinity]. Useful for cost functions.
+  return 2.0 / (1 + exp(-x)) - 1.0;
+
+}
+
+double getRunningAverage(int lane, double score, int max_num){
+
+	//cout << "getRunningAverage" << endl;
+
+	vector<double> scores;
+
+	if(!lane_wise_score.empty())
+		scores = lane_wise_score[lane];
+
+	if(scores.empty()){
+		for(int i=0; i < max_num;i++){
+			scores.push_back(score);
+		}
+
+	}else if(scores.size() < max_num){
+		for(int i=scores.size()-1; i < max_num-1;i++){
+			scores.push_back(score);
+		}
+
+		scores.push_back(score);
+	}else{
+		scores.erase(scores.begin());
+		scores.push_back(score);
+	}
+
+	double total = 0.0;
+
+	for(int i=0;i<scores.size();i++){
+		total += scores[i];
+	}
+
+	lane_wise_score[lane] = scores;
+
+	double average = total/scores.size();
+
+	//cout << "average score : " << average << end;
+
+	return average;
+
+
+}
+
+vector<vector<double>> costFunction(double s, int lane, vector<vector<double>> sensor_fusion, double car_speed) {
+
+	vector <double> scores = {0,0,0};
+    vector <double> front_vehicle;
+    vector <double> back_vehicle;
+    map<int, vector<vector<double>>> lane_wise_front_rear_vehicle;
+
+    int buffer_dist_per_car = PREFERRED_BUFFER_DISTANCE/2;
+
+    /***
+     * Cost function is based on three Four factors.
+     * A. Same lane cost: this help in differentiating between current lane and other lane having same score.
+     *    This will give boost to stay in the lane if speed is good and front vehicle is very far.
+     * B. Distance between front and back vehicle(roughly 100 m): If distance between front and back vehicle is very far
+     *    This will give score boost of +10.
+     *
+     *    In case of less than  less than 100 m, relative boosting wll be provided to score based on the speed and distance
+     *    between front car and rear car.
+     *
+     * C. Collision cost: In case of front or back car below the PREFERRED_BUFFER_DISTANCE, will be penalized by -10.
+     *
+     *
+     *
+     */
+
+
+    for (int i = 0; i < 3; i++) {
+	    if (i == lane) {  // benefit to keeping lane
+	      scores[i] += 0.5;
+	    }
+
+	    front_vehicle = closestVehicle(s, i, sensor_fusion, true);
+	    back_vehicle = closestVehicle(s, i, sensor_fusion, false);
+	    lane_wise_front_rear_vehicle[i] = {front_vehicle, back_vehicle};
+
+	    if (front_vehicle[0] > 1000 and back_vehicle[0] > 1000) {
+	      scores[i] += 10; // if wide open lane, move into that lane
+	    } else {
+	      if (front_vehicle[0] < buffer_dist_per_car) {
+	        scores[i] -= 10; // if car too close in front, negative score
+	      }
+	      if (back_vehicle[0] < buffer_dist_per_car) {
+	        scores[i] -= 10; // if car too close in back, negative score
+
+	      }
+
+
+
+	      //if front car is faster than our car, boost the score.
+	      //if back is slower than our car, boost the score Otherwise, reduce the score.
+
+	      //If front and back car are not too far(roughtly 100 m), then simple factors used for cost
+	      //evaluation.
+
+	      //PREFERRED_BUFFER_DISTANCE = 20 m. this is preferred combined gap between front and back car.
+	      // buffer_dist_per_car = PREFERRED_BUFFER_DISTANCE/2
+	      //a. farther the car, better it is. there, checking how far relative from buffer_dist_per_car and
+	      //   score will be boasted relatively.
+	      //b. if front car is faster in velocity, score will be boasted as it is directly propositions
+	      //   if back car is slower in velocity, score will be boasted as it is indirectly proportional.
+
+	      scores[i]   += 1- (buffer_dist_per_car / (front_vehicle[1]));
+		  scores[i]   += 1 / (back_vehicle[1]);
+	      scores[i]   += 1- (buffer_dist_per_car/(front_vehicle[0])); // benefit for large open distance in lane in front
+	      scores[i]   += 1- (buffer_dist_per_car/(back_vehicle[0])); // benefit for large open distance in lane in back
+	    }
+
+	    //Maintaining average of scores that will smooth out the swinging.
+	    //Otherwise, if there is frequent lane changes identified based on the cost function
+	    //it will cause too much jerk. Therefore, maintaining average.
+
+	    //avg_scores[i] = getRunningAverage(i, scores[i], AVERAGE_MAX_NUM);
+	    avg_scores[i] = ((avg_scores[i] * AVERAGE_MAX_NUM) - avg_scores[i]);
+	    avg_scores[i] += scores[i];
+	    avg_scores[i] /= AVERAGE_MAX_NUM;
+	  }
+
+
+     //below choosing the lane.
+
+	  // Only compare applicable lanes
+      double best_score;// = 100000.00;
+      int best_lane = 0;
+      vector<double> best_front_vehicle, best_back_vehicle;
+	  if (lane == 0) {
+	    best_score = avg_scores[0];
+	    best_front_vehicle = lane_wise_front_rear_vehicle.find(0)->second[0];
+	    best_back_vehicle = lane_wise_front_rear_vehicle.find(0)->second[1];
+	    if(avg_scores[lane] > avg_scores[lane+1])
+	    	best_lane = lane;
+	    else
+	    	best_lane = lane+1;
+	    //best_lane = max_element(avg_scores.begin(), avg_scores.end() - 1) - avg_scores.begin();
+	  } else if (lane == 1) {
+	    best_score = avg_scores[1];
+	    best_front_vehicle = lane_wise_front_rear_vehicle.find(1)->second[0];
+	    best_back_vehicle = lane_wise_front_rear_vehicle.find(1)->second[1];
+	    int best_lane = lane;
+	    /*
+	    for(int i = 0; i< avg_scores.size();i++){
+	    	if(avg_scores[i] > avg_scores[lane])
+	    		best_lane = avg_scores[i];
+	    }
+	    */
+	    best_lane = max_element(avg_scores.begin(), avg_scores.end())  - avg_scores.begin();
+	  } else {
+	    best_score = avg_scores[2];
+	    best_front_vehicle = lane_wise_front_rear_vehicle.find(2)->second[0];
+	    best_back_vehicle = lane_wise_front_rear_vehicle.find(2)->second[1];
+
+	    if(avg_scores[lane] > avg_scores[lane-1])
+	    	best_lane = lane;
+	    else
+	    	best_lane = lane-1;
+
+	    //best_lane = max_element(avg_scores.begin() + 1, avg_scores.end())  - avg_scores.begin();
+	  }
+
+/*
+	  //check before shifting or reduce speed if with in same lane.
+		front_vehicle = closestVehicle(s, best_lane, sensor_fusion, true);
+		back_vehicle = closestVehicle(s, best_lane, sensor_fusion, false);
+
+		// Reset to current lane and leading vehicle if not enough room
+		if (front_vehicle[0] < buffer_dist_per_car or back_vehicle[0] < buffer_dist_per_car or avg_scores[best_lane] <= -5) {
+			//cout << "============not changing lane================" << endl;
+			best_score = avg_scores[lane];
+			best_lane = lane;
+			front_vehicle = lane_wise_front_rear_vehicle.find(lane)->second[0];
+			back_vehicle = lane_wise_front_rear_vehicle.find(lane)->second[1];
+		    best_front_vehicle = front_vehicle;
+		    best_back_vehicle = back_vehicle;
+		}
+*/
+	  cout << "current lane: "<< lane <<  " , " << avg_scores[lane] <<" best lane: "<< best_lane << " , " << best_score << endl;
+
+	  return {{best_score}, {best_lane},best_front_vehicle,best_back_vehicle};
+
+}
+
+int getNextStateLaneFactor(double s, double d, vector<vector<double>> sensor_fusion, double car_speed) {
+
+  int lane = getLaneNumber(d);
+  int new_lane;
+
+  //get fron vehicle
+  vector<double> front_car = closestVehicle(s, lane, sensor_fusion, true);
+  double distance = front_car[0];
+
+  curr_lane = lane; // Keep the current lane to later calculate desired move
+
+  // check if blocked, i.e. car is within PREFERRED_BUFFER_DISTANCE
+  // If there is large distance between our car and front car
+  // then, speed will be maintained at around 49 miles/hr.Otherwise,
+  // Cost function will be invoked to identify the lane to stay in.
+  if (distance > PREFERRED_BUFFER_DISTANCE) {
+    new_lane = curr_lane;
+    target_vehicle_speed = PREFERRED_VELOCITY;
+    avg_scores = {0,0,0};
+    return 0;//new_lane;
+  } else {
+	//cost function return {best_score, best_lane to stay, best lane's front vehicle and rare vehicle.
+	vector<vector<double>> cost_param = costFunction(s, lane, sensor_fusion,car_speed);
+    new_lane = cost_param[1][0];
+    vector <double> front_vehicle = cost_param[2];
+    if(front_vehicle[0] < PREFERRED_BUFFER_DISTANCE/2)
+    	target_vehicle_speed = front_vehicle[1]/2;
+    else
+    	target_vehicle_speed = front_vehicle[1];
+    //cout << "=======target vehicle speed: " << target_vehicle_speed << endl;
+  }
+
+  //return new_lane;
+  //cout << "getNextStateLane: Nextlane: " << new_lane << " target vehicle speed: " << target_vehicle_speed << endl;
+
+  // To keep vehicle in center, offset will be set to either -4 or +4
+  // in case of moving to next lanes. If same lane, no offset required.
+
+  if (new_lane == lane) {
+    return 0;
+  } else if (new_lane < lane) {
+    return -4;
+  } else {
+    return 4;
+  }
+
+}
+
+//================================================================================================
+
 int main() {
   uWS::Hub h;
 
@@ -238,8 +560,152 @@ int main() {
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
-
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            // Start with remaining old path
+          	int path_size = previous_path_x.size();
+			for(int i = 0; i < path_size; i++)
+			{
+			  next_x_vals.push_back(previous_path_x[i]);
+			  next_y_vals.push_back(previous_path_y[i]);
+			}
+
+			vector<double> ptsx;
+			vector<double> ptsy;
+
+			double ref_x = car_x;
+			double ref_y = car_y;
+			double ref_yaw = deg2rad(car_yaw);
+			double ref_vel;
+
+			// If no previous path, initiate at current values
+
+			if(path_size < 2)
+			{
+			  double prev_car_x = car_x - cos(car_yaw);
+			  double prev_car_y = car_y - sin(car_yaw);
+			  ptsx.push_back(prev_car_x);
+			  ptsx.push_back(car_x);
+			  ptsy.push_back(prev_car_y);
+			  ptsy.push_back(car_y);
+			  ref_vel = car_speed;
+			}
+			else  // Otherwise, use previous x and y and calculate angle based on change in x & y
+			{
+			  ref_x = previous_path_x[path_size-1];
+			  ref_y = previous_path_y[path_size-1];
+			  double ref_x_prev = previous_path_x[path_size-2];
+			  double ref_y_prev = previous_path_y[path_size-2];
+			  ref_yaw = atan2(ref_y-ref_y_prev,ref_x-ref_x_prev);
+			  ref_vel = target_vehicle_speed;
+			  // Append starter points for spline later
+
+			  ptsx.push_back(ref_x_prev);
+			  ptsx.push_back(ref_x);
+			  ptsy.push_back(ref_y_prev);
+			  ptsy.push_back(ref_y);
+			}
+			// Plan the rest of the path based on calculations
+
+
+			vector<double> frenet_vec = getFrenet(ref_x, ref_y, ref_yaw, map_waypoints_x, map_waypoints_y);
+			curr_lane = getLaneNumber(frenet_vec[1]);
+			double moveLaneOffset = getNextStateLaneFactor(frenet_vec[0], frenet_vec[1], sensor_fusion,car_speed);
+			double lane = curr_lane;
+
+			//double next_d = getD(moveLane);
+
+			double next_d = (lane * 4) + 2 + moveLaneOffset;
+
+
+			// Double-check that the car has not incorrectly chose a blocked lane
+
+			int check_lane = getLaneNumber(next_d);
+			vector<double> front_vehicle = closestVehicle(frenet_vec[0], check_lane, sensor_fusion, true);
+			vector<double> back_vehicle = closestVehicle(frenet_vec[0], check_lane, sensor_fusion, false);
+
+			// Reset to current lane and leading vehicle if not enough room
+			if (front_vehicle[0] < PREFERRED_BUFFER_DISTANCE/2 or back_vehicle[0] < PREFERRED_BUFFER_DISTANCE/2 or avg_scores[check_lane] <= -5) {
+				next_d =  4 * lane + 2;
+				lane = getLaneNumber(next_d);
+				//next_d = getD(lane);
+			    if (check_lane != lane) {
+				  front_vehicle = closestVehicle(frenet_vec[0], lane, sensor_fusion, true);
+   			      target_vehicle_speed = front_vehicle[1];
+			    }
+			}
+
+			int first, second, third;
+			third = 150;
+			second = 100;
+			first = 50;
+
+			// Set further waypoints based on going further along highway in desired lane
+			vector <double> wp1 = getXY(car_s+first, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector <double> wp2 = getXY(car_s+second, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector <double> wp3 = getXY(car_s+third, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			ptsx.push_back(wp1[0]);
+			ptsx.push_back(wp2[0]);
+			ptsx.push_back(wp3[0]);
+
+			ptsy.push_back(wp1[1]);
+			ptsy.push_back(wp2[1]);
+			ptsy.push_back(wp3[1]);
+
+
+			if (ptsx.size() > 2) {  // Spline fails if not greater than two points - Otherwise just use rest of old path
+			  // Shift and rotate points to local coordinates
+			  for (int i = 0; i < ptsx.size(); i++) {
+				double shift_x = ptsx[i] - ref_x;
+				double shift_y = ptsy[i] - ref_y;
+				ptsx[i] = (shift_x*cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+				ptsy[i] = (shift_x*sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+
+			  }
+			  // create a spline
+
+			  tk::spline s;
+			  // set (x,y) points to the spline
+			  s.set_points(ptsx, ptsy);
+
+			  double target_x = 30;
+			  double target_y = s(target_x);
+			  double target_dist = sqrt(pow(target_x,2)+pow(target_y,2));
+			  double x_add_on = 0;
+			  const int MAX_ACCEL= 10; // m/s/s
+			  const double accel = (MAX_ACCEL) * 0.02 * 0.8; // Limit acceleration within acceptable range
+
+			  //cout << "ref_vel: " << ref_vel << endl;
+
+			  for(int i = 0; i < 50 - path_size; i++) {
+				if (ref_vel < target_vehicle_speed - accel) {  // Accelerate if under target speed
+				  ref_vel += accel;
+				} else if (ref_vel > target_vehicle_speed + accel) { // Brake if below target
+				  ref_vel -= accel;
+				}
+
+				// Calculate points along new path
+
+				double N = (target_dist/(.02*ref_vel));
+				double x_point = x_add_on+(target_x)/N;
+				double y_point = s(x_point);
+
+				x_add_on = x_point;
+				double x_ref = x_point;
+				double y_ref = y_point;
+
+				// Rotate and shift back to normal
+				x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+				y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+				x_point += ref_x;
+				y_point += ref_y;
+
+				next_x_vals.push_back(x_point);
+				next_y_vals.push_back(y_point);
+			  }
+			}
+			target_vehicle_speed = ref_vel;  // Save the end speed to be used for the next path
+
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
@@ -290,3 +756,5 @@ int main() {
   }
   h.run();
 }
+
+
---
+++ writeup.md
@@ -0,0 +1,185 @@
+# CarND-Path-Planning-Project
+Self-Driving Car Engineer Nanodegree Program
+
+This writing is for Term3 Path Planning project.  All minimum requirements mentioned below are met
+
+1. Car has travelled more than 6.72 miles and has some collision issue that can be further analyzed and fixed.
+2. Maximum Acceleration and Jerk limit were met.
+3. No collision before 6.72 miles.
+4. Car stay within restricted lanes(i.e. lanes in car driving direction).
+5. Car changes lanes when front car is blocking it and when immediate next lanes(left/right) are available for driving without collision.
+
+
+### Approach
+
+## Trajectory Generations
+
+* Trajectory generation was done based on following steps
+  * **Generate first set of trajectory points:** Based on previous path trajectory points availability, either new points are generated or previouly generated trajectory points will be picked up for smooth trajectory generation.
+  * **Identify Lane:** Last trajectory point from previous step will be considered as current location and nextState will be identified. This will be trigger either front vehicle is with in PREFERRED BUFFER DISTANCE(called as PBD)(20 m).If more than PBD, cost function will be invoked. Cost function is based on 
+    	* Distance between front and back vehicle: More the distance cost gets boasting.
+    	* front vehicle speed: If front vehicle is faster than our car, cost gets boasting.
+    	* back vehicle speed: If back vehicle is slower than our car, cost get boasting.
+    	* car is within PBD and < 10 m, cost get hit.
+    	* Staying in same lane: If cost for staying in same lane and changing to other lane is same, it might cause swings. Therefore, boasting car if it stays in same lane will help.
+    Now, after finding lane based on above cost, to avoid frequent car change, cost averaging will be done 
+    
+    NOTE: Even though, lane identification done based on average cost, one final check will be done to make sure no front/back car is within 10 m to avoid collision. If found, lane change guidance provided by cost function will be overridden and car will stay in same lane.
+    
+  * **Further trajectory point generation:** Further trajectory points will be generate using **spline** library.
+  
+  
+### Below is link video of car driving 6.72 miles.
+ 
+  [Car Traveling 6.72 miles](https://youtu.be/pMbigmGmz0A)
+ 
+### What further can be done
+
+ * Cost function can be improved further for inclusion of jerk cost.
+ * Instead of spline library JERK MINIMIZATION POLYNOMIAL can be utilized.
+ * two cost functions. 1) Behavior planning cost(not used)  2) Trajectory generation cost(current used). Behvior planning cost function not used primarily because of "scheduling compute time". Behavior module inputs were provided less frequent than localization module input for generating trajectories.
+ * Trajectories can be further sent to MPC module.
+ 
+ 
+    
+
+
+
+   
+### Simulator.
+You can download the Term3 Simulator which contains the Path Planning Project from the [releases tab (https://github.com/udacity/self-driving-car-sim/releases).
+
+### Goals
+In this project your goal is to safely navigate around a virtual highway with other traffic that is driving +-10 MPH of the 50 MPH speed limit. You will be provided the car's localization and sensor fusion data, there is also a sparse map list of waypoints around the highway. The car should try to go as close as possible to the 50 MPH speed limit, which means passing slower traffic when possible, note that other cars will try to change lanes too. The car should avoid hitting other cars at all cost as well as driving inside of the marked road lanes at all times, unless going from one lane to another. The car should be able to make one complete loop around the 6946m highway. Since the car is trying to go 50 MPH, it should take a little over 5 minutes to complete 1 loop. Also the car should not experience total acceleration over 10 m/s^2 and jerk that is greater than 50 m/s^3.
+
+#### The map of the highway is in data/highway_map.txt
+Each waypoint in the list contains  [x,y,s,dx,dy] values. x and y are the waypoint's map coordinate position, the s value is the distance along the road to get to that waypoint in meters, the dx and dy values define the unit normal vector pointing outward of the highway loop.
+
+The highway's waypoints loop around so the frenet s value, distance along the road, goes from 0 to 6945.554.
+
+## Basic Build Instructions
+
+1. Clone this repo.
+2. Make a build directory: `mkdir build && cd build`
+3. Compile: `cmake .. && make`
+4. Run it: `./path_planning`.
+
+Here is the data provided from the Simulator to the C++ Program
+
+#### Main car's localization Data (No Noise)
+
+["x"] The car's x position in map coordinates
+
+["y"] The car's y position in map coordinates
+
+["s"] The car's s position in frenet coordinates
+
+["d"] The car's d position in frenet coordinates
+
+["yaw"] The car's yaw angle in the map
+
+["speed"] The car's speed in MPH
+
+#### Previous path data given to the Planner
+
+//Note: Return the previous list but with processed points removed, can be a nice tool to show how far along
+the path has processed since last time. 
+
+["previous_path_x"] The previous list of x points previously given to the simulator
+
+["previous_path_y"] The previous list of y points previously given to the simulator
+
+#### Previous path's end s and d values 
+
+["end_path_s"] The previous list's last point's frenet s value
+
+["end_path_d"] The previous list's last point's frenet d value
+
+#### Sensor Fusion Data, a list of all other car's attributes on the same side of the road. (No Noise)
+
+["sensor_fusion"] A 2d vector of cars and then that car's [car's unique ID, car's x position in map coordinates, car's y position in map coordinates, car's x velocity in m/s, car's y velocity in m/s, car's s position in frenet coordinates, car's d position in frenet coordinates. 
+
+## Details
+
+1. The car uses a perfect controller and will visit every (x,y) point it recieves in the list every .02 seconds. The units for the (x,y) points are in meters and the spacing of the points determines the speed of the car. The vector going from a point to the next point in the list dictates the angle of the car. Acceleration both in the tangential and normal directions is measured along with the jerk, the rate of change of total Acceleration. The (x,y) point paths that the planner recieves should not have a total acceleration that goes over 10 m/s^2, also the jerk should not go over 50 m/s^3. (NOTE: As this is BETA, these requirements might change. Also currently jerk is over a .02 second interval, it would probably be better to average total acceleration over 1 second and measure jerk from that.
+
+2. There will be some latency between the simulator running and the path planner returning a path, with optimized code usually its not very long maybe just 1-3 time steps. During this delay the simulator will continue using points that it was last given, because of this its a good idea to store the last points you have used so you can have a smooth transition. previous_path_x, and previous_path_y can be helpful for this transition since they show the last points given to the simulator controller with the processed points already removed. You would either return a path that extends this previous path or make sure to create a new path that has a smooth transition with this last path.
+
+## Tips
+
+A really helpful resource for doing this project and creating smooth trajectories was using http://kluge.in-chemnitz.de/opensource/spline/, the spline function is in a single hearder file is really easy to use.
+
+---
+
+## Dependencies
+
+* cmake >= 3.5
+ * All OSes: [click here for installation instructions](https://cmake.org/install/)
+* make >= 4.1
+  * Linux: make is installed by default on most Linux distros
+  * Mac: [install Xcode command line tools to get make](https://developer.apple.com/xcode/features/)
+  * Windows: [Click here for installation instructions](http://gnuwin32.sourceforge.net/packages/make.htm)
+* gcc/g++ >= 5.4
+  * Linux: gcc / g++ is installed by default on most Linux distros
+  * Mac: same deal as make - [install Xcode command line tools]((https://developer.apple.com/xcode/features/)
+  * Windows: recommend using [MinGW](http://www.mingw.org/)
+* [uWebSockets](https://github.com/uWebSockets/uWebSockets)
+  * Run either `install-mac.sh` or `install-ubuntu.sh`.
+  * If you install from source, checkout to commit `e94b6e1`, i.e.
+    ```
+    git clone https://github.com/uWebSockets/uWebSockets 
+    cd uWebSockets
+    git checkout e94b6e1
+    ```
+
+## Editor Settings
+
+We've purposefully kept editor configuration files out of this repo in order to
+keep it as simple and environment agnostic as possible. However, we recommend
+using the following settings:
+
+* indent using spaces
+* set tab width to 2 spaces (keeps the matrices in source code aligned)
+
+## Code Style
+
+Please (do your best to) stick to [Google's C++ style guide](https://google.github.io/styleguide/cppguide.html).
+
+## Project Instructions and Rubric
+
+Note: regardless of the changes you make, your project must be buildable using
+cmake and make!
+
+
+## Call for IDE Profiles Pull Requests
+
+Help your fellow students!
+
+We decided to create Makefiles with cmake to keep this project as platform
+agnostic as possible. Similarly, we omitted IDE profiles in order to ensure
+that students don't feel pressured to use one IDE or another.
+
+However! I'd love to help people get up and running with their IDEs of choice.
+If you've created a profile for an IDE that you think other students would
+appreciate, we'd love to have you add the requisite profile files and
+instructions to ide_profiles/. For example if you wanted to add a VS Code
+profile, you'd add:
+
+* /ide_profiles/vscode/.vscode
+* /ide_profiles/vscode/README.md
+
+The README should explain what the profile does, how to take advantage of it,
+and how to install it.
+
+Frankly, I've never been involved in a project with multiple IDE profiles
+before. I believe the best way to handle this would be to keep them out of the
+repo root to avoid clutter. My expectation is that most profiles will include
+instructions to copy files to a new location to get picked up by the IDE, but
+that's just a guess.
+
+One last note here: regardless of the IDE used, every submitted project must
+still be compilable with cmake and make./
+
+## How to write a README
+A well written README file can enhance your project and portfolio.  Develop your abilities to create professional README files by completing [this free course](https://www.udacity.com/course/writing-readmes--ud777).
+
